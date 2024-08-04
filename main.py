df5accc6e12c6d0057f5b0461f0bc7079213ac92
SystemTraveler <adobedays614@gmail.com>
2024-08-04 16:26:03 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,24 +4,7 @@ import xml.etree.ElementTree as ET
 
 def paint_pixel(x, y, color):
     url = "https://api.pixelbattles.ru/pix"
-    headers = {
-        "Accept": "*/*",
-        "Accept-Encoding": "gzip, deflate, br, zstd",
-        "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
-        "Connection": "keep-alive",
-        "Content-Type": "application/json",
-        "Cookie": "token=c33ffb39-87f8-4f55-86fd-4c88a30ea8a4; _ym_uid=1722712700144760144; _ym_d=1722712700; _ym_isad=1",
-        "Host": "api.pixelbattles.ru",
-        "Origin": "https://pixelbattles.ru",
-        "Referer": "https://pixelbattles.ru/",
-        "Sec-Fetch-Dest": "empty",
-        "Sec-Fetch-Mode": "cors",
-        "Sec-Fetch-Site": "same-site",
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
-        "sec-ch-ua": "\"Not)A;Brand\";v=\"99\", \"Google Chrome\";v=\"127\", \"Chromium\";v=\"127\"",
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": "\"macOS\""
-    }
+    headers = {}
 
     data = {
         "x": x,
